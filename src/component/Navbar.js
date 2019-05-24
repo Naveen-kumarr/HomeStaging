f8496c390e6403249737efcbc8ba222dae50a925
naveen <naveen>
2019-05-24 18:21:07 +0530

    Design Updated

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,18 +8,18 @@ return(
 <div>
 <div className="sticky-top">
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand mobile" href="#">Navbar</a>
+  <a className="navbar-brand mobile colorchange" href="#">Navbar</a>
   <img src={Logo} className="navbar-brand desktopnone" style={{width:"50px",height:"50px"}}/>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <button className="navbar-toggler customtoggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNavDropdown">
     <ul className="navbar-nav">
       <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        <a className="nav-link colorchange" href="#">Home <span className="sr-only">(current)</span></a>
       </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
+      <li className="nav-item ">
+        <a className="nav-link colorchange" href="#">Features</a>
       </li>
 	   </ul>
 	   
@@ -29,12 +29,12 @@ return(
 	   
 	  	  <ul className="navbar-nav">
       <li className="nav-item ">
-        <a className="nav-link" href="#">Pricing</a>
+        <a className="nav-link colorchange" href="#">Pricing</a>
       </li>
 	 
 
        <li className="nav-item ">
-        <a className="nav-link" href="#">Anything</a>
+        <a className="nav-link colorchange" href="#">Anything</a>
       </li>
     </ul>
   </div>
